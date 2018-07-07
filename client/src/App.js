5b80524e7b0aa051578dfe65e55013fc34ebb05e
Adrian de Leon <adeleon@linode.com>
2018-07-07 08:56:33 -0400

    refactor main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card from './Card.js'
 import axios from 'axios'
 
-class App extends Component { 
+class App extends Component {
   // eslint-disable-next-line
-  state = {
-    data: [
-      {title: 'hey', dateDisplay: 'hey', excerpt: 'hey', permalink: 'hey', imageSrc: 'hey'}
-    ],
-    isLoaded: false
-  } 
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      data: [],
+      isLoaded: false,
+    }
+  }
 
   componentDidMount() {
-    axios.get('/data')
-      .then( res => {
-        const data = res.data
-        this.setState(prevState => {
-          return {
-            data,
-            isLoaded: true
-          }
-        })
+    axios.get('/data').then(result => {
+      this.setState(prevState => {
+        return {
+          isLoaded: true,
+          data: [...prevState.data, ...result.data],
+        }
       })
+    })
   }
+
   render() {
-    const isLoaded = this.state.isLoaded
-      const [{title, dateDisplay, excerpt, permalink, imageSrc}] = this.state.data
-    
-    return (
-      <div>
-        {isLoaded ?
-            this.state.data.map(({title, dateDisplay, excerpt, permalink, imageSrc}) =>   
+    const { error, isLoaded, data } = this.state
+
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <h1>Loading...</h1>
+    } else {
+      return data.map(
+        ({ title, dateDisplay, excerpt, permalink, imageSrc }) => {
+          return (
             <Card
               title={title}
               dateDisplay={dateDisplay}
               excerpt={excerpt}
               permalink={permalink}
               imageSrc={imageSrc}
-              key={title + dateDisplay}/>
-            )
-            : <h1>loading</h1>}
-      </div>
-    )
+              key={title + dateDisplay}
+            />
+          )
+        }
+      )
+    }
   }
 }
 
