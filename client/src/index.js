7e30297de08f1f9d5688e770103200fecd076c30
Adrian de Leon <adeleon@linode.com>
2018-07-11 05:08:06 -0400

    refactor for clarity

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'tachyons/css/tachyons.min.css'
+import './colors.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
--- index.js
+++ index.js
@@ -7,29 +7,32 @@ const app = express()
 
 let data
 let objData
-const re =  /{"eventId":.*}/gi
+const re = /{"eventId":.*}/gi
 
-const getData = async (callback) => {
+const getData = async callback => {
   try {
     data = await axios.get('https://phillyimprovtheater.com/shows/')
-    data = data.data.replace(/&quot;/g, '\"')
-    data = data.replace(/"i18n":{"find_out_more":"Find out more \\u00bb","for_date":"Events for"},/g,'')
+    data = data.data.replace(/&quot;/g, '"')
+    data = data.replace(
+      /"i18n":{"find_out_more":"Find out more \\u00bb","for_date":"Events for"},/g,
+      ''
+    )
     data = data.match(re)
     objData = data.map((x, i) => {
-       x = JSON.parse(x)
-       return x
-     })
+      x = JSON.parse(x)
+      return x
+    })
     callback()
-
   } catch (error) {
     console.error(error)
   }
 }
 
-getData(() => console.log('Data pulled'))
+if (!objData) {
+  getData(() => console.log('Data pulled'))
+}
 
 app.get('/', (req, res) => res.send('Welcome to the improved calendar'))
 app.get('/data', (req, res) => res.send(objData))
 
-
-app.listen(4000, () => console.log("Listening on port 4000"))
+app.listen(4000, () => console.log('Listening on port 4000'))
