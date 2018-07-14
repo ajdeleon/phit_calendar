bcc5e2146742e98ddca42145f7bde26bcc42632d
Adrian de Leon <adeleon@linode.com>
2018-07-14 04:12:50 -0400

    add footer for basic info

---
+++ client/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="tc ma4 f4" role="contentinfo">
+    This project is meant to provide an accessible version of
+    phitcomedy.com/shows. To learn more about web accessibility please visit
+    <a className="site-blue" href="https://www.a11yproject.com/about">
+      {' '}
+      a11yproject.com
+    </a>
+  </footer>
+)
+
+export default Footer
