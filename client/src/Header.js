d4ee349b75478fc7a813ed174f0a7fb528c27bfb
Adrian de Leon <adeleon@linode.com>
2018-07-14 04:21:12 -0400

    add header with site title

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => (
+  <header role="banner" className="tc f2 b">
+    Accessible calendar for{' '}
+    <a className="site-blue" href="https://www.phitcomedy.com">
+      phitcomedy.com
+    </a>
+  </header>
+)
+
+export default Header
