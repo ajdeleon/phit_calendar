b236e6d5e2d60b205e50439861373eb884087310
Adrian de Leon <adeleon@linode.com>
2018-07-22 08:37:36 -0400

    add function to convert string date to utc

---
+++ dateConverter.js
@@ -0,0 +1,34 @@
+module.exports = function(data) {
+  const dataCopy = [...data]
+
+  const dateConverter = {
+    January: 1,
+    February: 2,
+    March: 3,
+    April: 4,
+    May: 5,
+    June: 6,
+    July: 7,
+    August: 8,
+    September: 9,
+    October: 10,
+    November: 11,
+    December: 12,
+  }
+
+  const unchangedDatesArray = data.map(x => {
+    return x.endTime.split(' ').slice(0, 2)
+  })
+
+  const convertedDates = unchangedDatesArray.map((x, i) => {
+    return new Date(`2018-${dateConverter[x[0]]}-${x[1]}`)
+  })
+
+  const convertedData = dataCopy
+    .map((x, i) => {
+      return { ...x, endTimeConverted: convertedDates[i] }
+    })
+    .filter(show => show.endTimeConverted > new Date())
+
+  return convertedData
+}
