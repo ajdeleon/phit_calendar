0c6f7e66ae6edb011c6233037c98e3bf2c9b0a79
Adrian de Leon <ajdeleon1064@gmail.com>
2018-08-29 16:44:16 -0400

    add button to toggle about box

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Footer = () => (
-  <footer className="tc ma4 f4" role="contentinfo">
-    This project is meant to provide an accessible version of
-    phitcomedy.com/shows. To learn more about web accessibility please visit
-    <a className="site-blue" href="https://www.a11yproject.com/about">
-      {' '}
-      a11yproject.com
-    </a>
-  </footer>
-)
+class Footer extends Component {
+  // eslint-disable-next-line react/parsing-error
+  state = {
+    isOpen: false,
+  }
+
+  toggleIsOpen = () => {
+    this.setState(state => {
+      return {
+        isOpen: !state.isOpen,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <footer className="tc ma4 f4" role="contentinfo">
+        <button onClick={this.toggleIsOpen} type="button">
+          About
+        </button>
+        {this.state.isOpen ? (
+          <div>
+            <p>
+              {' '}
+              This project is meant to provide an accessible version of
+              phitcomedy.com/shows. To learn more about web accessibility please
+              visit
+            </p>
+            <a className="site-blue" href="https://www.a11yproject.com/about">
+              {' '}
+              a11yproject.com
+            </a>
+          </div>
+        ) : (
+          <div style={{ height: '4rem' }} />
+        )}{' '}
+      </footer>
+    )
+  }
+}
 
 export default Footer
