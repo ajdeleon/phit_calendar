b83a148c51a8f1bbdb187960dfbebc3752bab70b
AJ de Leon <ajdeleon1064@gmail.com>
2019-01-11 23:36:39 +0000

    show site down message

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,6 +6,7 @@ const Header = () => (
     <a className="site-blue" href="https://www.phitcomedy.com">
       phitcomedy.com
     </a>
+  <p>As of December 2018 this no longer works. Something changed on the backend that is preventing my parsing from working correctly. I will implement with a more robust solution like cheerio js at some point.</p>
   </header>
 )
 
